fb4341c9934b4956e4fe26e8bc6b5519a25d8a2b
alvaroormeno <alvaro@ormeno.org>
2022-07-22 17:28:22 -0400

    express and my sql connection complete

---
+++ server/index.js
@@ -0,0 +1,44 @@
+// set up express server
+// Step 1 - require express to have instance of express library
+const express = require('express')
+// Step 2 - create app from that instance
+const app = express()
+
+
+// set up mysql
+// STEP 1 - import mysql
+const mysql = require('mysql')
+//  Step 2 Connect to DB
+const db = mysql.createConnection({
+    user: 'root',
+    host: "localhost",
+    password: 'Mysqlpass',
+    database: "HRapp",
+
+    
+})
+
+
+app.post("/create", (req, res) => {
+    const name = req.body.name;
+    const age = req.body.age;
+    const country = req.body.country;
+    const position = req.body.position;
+    const wage = req.body.wage;
+
+    db.query('INSERT INTO employees (name, age, country, position, wage) VALUES(?,?,?,?,?)',
+    [name, age, country, position, wage], (err, result) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.send('values inserted')
+        }
+    }
+    )
+
+})
+
+// Step 3 (express) - start app by listening to port
+app.listen(3001, () => {
+    console.log("server is running on port 3001!")
+})
